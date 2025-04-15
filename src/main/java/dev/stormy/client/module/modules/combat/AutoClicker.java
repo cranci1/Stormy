a5eecf6cd006a174e58b84a5f06c86ffca6a140e
Francesco <100066266+cranci1@users.noreply.github.com>
2025-04-15 14:53:35 +0200

    Revert "g"
    This reverts commit d2cac09a042c3143ad7487eb48e7144d6f991333.

--- src/main/java/dev/stormy/client/module/modules/combat/AutoClicker.java
+++ src/main/java/dev/stormy/client/module/modules/combat/AutoClicker.java
@@ -1,8 +1,10 @@
 package dev.stormy.client.module.modules.combat;
 
 import dev.stormy.client.module.Module;
-import dev.stormy.client.module.modules.client.ArrayListModule.ColorModes;
-import dev.stormy.client.module.setting.impl.*;
+import dev.stormy.client.module.setting.impl.DescriptionSetting;
+import dev.stormy.client.module.setting.impl.SliderSetting;
+import dev.stormy.client.module.setting.impl.TickSetting;
+import dev.stormy.client.utils.Utils;
 import dev.stormy.client.utils.asm.HookUtils;
 import dev.stormy.client.utils.math.TimerUtils;
 import dev.stormy.client.utils.player.PlayerUtils;
@@ -12,188 +14,84 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemSword;
 import net.minecraft.util.BlockPos;
-import net.minecraft.util.MovingObjectPosition;
 import net.weavemc.loader.api.event.*;
 import org.lwjgl.input.Mouse;
+import net.minecraft.util.MovingObjectPosition;
 
 import java.util.Random;
-import java.util.concurrent.ThreadLocalRandom;
 
 @SuppressWarnings("unused")
 public class AutoClicker extends Module {
-    public enum ClickPattern {
-        NORMAL, BUTTERFLY, JITTER
-    }
-
-    public static ComboSetting<ClickPattern> clickPattern;
+    // Settings
     public static SliderSetting minCPS;
     public static SliderSetting maxCPS;
-    public static SliderSetting jitterStrength;
-    public static SliderSetting butterflyRandom;
+    public static SliderSetting jitterAmount;
     public static SliderSetting blockHitChance;
-    public static TickSetting smartBlock;
-    public static TickSetting randomizeClicks;
+    public static SliderSetting minBlockHitDelay;
+    public static SliderSetting maxBlockHitDelay;
     public static TickSetting breakBlocks;
     public static TickSetting hitSelect;
     public static TickSetting weaponOnly;
 
-    // Timing variables
-    private final TimerUtils clickTimer = new TimerUtils();
-    private final TimerUtils blockTimer = new TimerUtils();
-    private long nextClickDelay;
-    private long nextBlockDelay;
-    private boolean blocking = false;
-    private boolean isBreakingBlock = false;
+    // Click timing variables
+    private long nextPressTime = 0L;
+    private long nextReleaseTime = 0L;
+    private long nextMultiplierUpdateTime = 0L;
+    private long nextExtraDelayUpdateTime = 0L;
+    private long blockHitStartTime = 0L;
+    private double delayMultiplier = 1.0D;
+    private boolean multiplierActive = false;
+    private boolean isHoldingBlockBreak = false;
+    private boolean isBlockHitActive = false;
 
-    // Random generators
-    private final Random rand = ThreadLocalRandom.current();
-    private double randomizedDelay = 1.0;
-    private float currentJitter = 0.0f;
-
-    // Key bindings
-    private final int LMB = mc.gameSettings.keyBindAttack.getKeyCode();
-    private final int RMB = mc.gameSettings.keyBindUseItem.getKeyCode();
+    // Random and utility
+    private final Random rand = new Random();
+    private final TimerUtils timer = new TimerUtils();
+    private final int lmb = mc.gameSettings.keyBindAttack.getKeyCode();
+    private final int rmb = mc.gameSettings.keyBindUseItem.getKeyCode();
 
     public AutoClicker() {
         super("AutoClicker", ModuleCategory.Combat, 0);
-        this.registerSetting(new DescriptionSetting("Advanced AutoClicker with multiple patterns"));
-        this.registerSetting(clickPattern = new ComboSetting<>("Pattern", ClickPattern.NORMAL));
-        this.registerSetting(minCPS = new SliderSetting("Min CPS", 9.0, 1.0, 20.0, 0.5));
-        this.registerSetting(maxCPS = new SliderSetting("Max CPS", 12.0, 1.0, 20.0, 0.5));
-        this.registerSetting(jitterStrength = new SliderSetting("Jitter Strength", 0.0, 0.0, 3.0, 0.1));
-        this.registerSetting(butterflyRandom = new SliderSetting("Butterfly Random", 0.85, 0.1, 1.0, 0.05));
-        this.registerSetting(blockHitChance = new SliderSetting("Block Chance", 100.0, 0.0, 100.0, 1.0));
-        this.registerSetting(smartBlock = new TickSetting("Smart Block", true));
-        this.registerSetting(randomizeClicks = new TickSetting("Randomize", true));
-        this.registerSetting(breakBlocks = new TickSetting("Break Blocks", true));
-        this.registerSetting(hitSelect = new TickSetting("Hit Select", true));
-        this.registerSetting(weaponOnly = new TickSetting("Weapon Only", true));
+        this.registerSetting(new DescriptionSetting("Click automatically with advanced options"));
+        this.registerSetting(minCPS = new SliderSetting("Min CPS", 9.0D, 1.0D, 20.0D, 0.5D));
+        this.registerSetting(maxCPS = new SliderSetting("Max CPS", 12.0D, 1.0D, 20.0D, 0.5D));
+        this.registerSetting(jitterAmount = new SliderSetting("Jitter", 0.0D, 0.0D, 3.0D, 0.1D));
+        this.registerSetting(blockHitChance = new SliderSetting("Block Hit %", 0.0D, 0.0D, 100.0D, 1.0D));
+        this.registerSetting(minBlockHitDelay = new SliderSetting("Min Block Hit Hold", 20.0D, 10.0D, 200.0D, 1.0D));
+        this.registerSetting(maxBlockHitDelay = new SliderSetting("Max Block Hit Hold", 40.0D, 10.0D, 200.0D, 1.0D));
+        this.registerSetting(breakBlocks = new TickSetting("Break blocks", false));
+        this.registerSetting(hitSelect = new TickSetting("Hit Select", false));
+        this.registerSetting(weaponOnly = new TickSetting("Weapon Only", false));
     }
 
-    @SubscribeEvent
-    public void onTick(TickEvent e) {
-        if (!isEnabled() || !PlayerUtils.isPlayerInGame() || mc.currentScreen != null)
-            return;
+    @Override
+    public void onEnable() {
+        this.isBlockHitActive = Mouse.isButtonDown(1);
+        resetClickTimers();
+    }
 
-        if (Mouse.isButtonDown(0)) {
-            if (!canClick())
-                return;
-            if (breakBlocks.isToggled() && handleBlockBreaking())
-                return;
+    @Override
+    public void onDisable() {
+        resetClickTimers();
+        this.isHoldingBlockBreak = false;
+        this.isBlockHitActive = false;
+    }
 
-            ClickPattern currentPattern = clickPattern.getMode(); // Get current pattern safely
-            switch (currentPattern) {
-                case NORMAL:
-                    normalClick();
-                    break;
-                case BUTTERFLY:
-                    butterflyClick();
-                    break;
-                case JITTER:
-                    jitterClick();
-                    break;
-            }
-        } else {
-            resetClicking();
+    private void resetClickTimers() {
+        this.nextPressTime = 0L;
+        this.nextReleaseTime = 0L;
+    }
+
+    public boolean shouldAttack() {
+        if (weaponOnly.isToggled() && !isHoldingWeapon()) {
+            return false;
         }
 
-        handleAutoBlock();
-    }
+        if (hitSelect.isToggled()) {
+            return hitSelectLogic();
+        }
 
-    private boolean canClick() {
-        if (weaponOnly.isToggled() && !isHoldingWeapon())
-            return false;
-        if (hitSelect.isToggled() && !isTargetValid())
-            return false;
         return true;
-    }
-
-    private void normalClick() {
-        if (clickTimer.hasReached(nextClickDelay)) {
-            simulateClick();
-            updateClickDelay();
-        }
-    }
-
-    private void butterflyClick() {
-        if (clickTimer.hasReached(nextClickDelay)) {
-            simulateClick();
-            if (rand.nextDouble() <= butterflyRandom.getInput()) {
-                simulateClick(); // Double click for butterfly effect
-            }
-            updateClickDelay();
-        }
-    }
-
-    private void jitterClick() {
-        if (clickTimer.hasReached(nextClickDelay)) {
-            applyJitter();
-            simulateClick();
-            updateClickDelay();
-        }
-    }
-
-    private void simulateClick() {
-        KeyBinding.setKeyBindState(LMB, true);
-        KeyBinding.onTick(LMB);
-        HookUtils.setMouseButtonState(0, true);
-
-        // Release after a small delay
-        ThreadLocalRandom.current().nextInt(1, 5);
-        KeyBinding.setKeyBindState(LMB, false);
-        HookUtils.setMouseButtonState(0, false);
-    }
-
-    private void handleAutoBlock() {
-        if (!smartBlock.isToggled() || !isHoldingWeapon())
-            return;
-
-        MovingObjectPosition mop = mc.objectMouseOver;
-        boolean shouldBlock = mop != null &&
-                mop.typeOfHit == MovingObjectPosition.MovingObjectType.ENTITY &&
-                mop.entityHit instanceof EntityPlayer &&
-                rand.nextDouble() * 100 < blockHitChance.getInput();
-
-        if (shouldBlock && !blocking && blockTimer.hasReached(nextBlockDelay)) {
-            KeyBinding.setKeyBindState(RMB, true);
-            KeyBinding.onTick(RMB);
-            blocking = true;
-            updateBlockDelay();
-        } else if (!shouldBlock && blocking) {
-            KeyBinding.setKeyBindState(RMB, false);
-            blocking = false;
-        }
-    }
-
-    private void updateClickDelay() {
-        double baseDelay = 1000.0 / (minCPS.getInput() +
-                (maxCPS.getInput() - minCPS.getInput()) * rand.nextDouble());
-
-        if (randomizeClicks.isToggled()) {
-            if (rand.nextInt(100) > 80) {
-                randomizedDelay = 1.0 + (rand.nextDouble() * 0.3);
-            }
-            baseDelay *= randomizedDelay;
-        }
-
-        nextClickDelay = (long) baseDelay;
-        clickTimer.reset();
-    }
-
-    private void updateBlockDelay() {
-        nextBlockDelay = 50L + rand.nextInt(100);
-        blockTimer.reset();
-    }
-
-    private void applyJitter() {
-        if (jitterStrength.getInput() <= 0)
-            return;
-
-        float strength = (float) jitterStrength.getInput();
-        currentJitter = (rand.nextFloat() - 0.5f) * strength;
-
-        mc.thePlayer.rotationYaw += currentJitter;
-        mc.thePlayer.rotationPitch += currentJitter * 0.5f;
     }
 
     private boolean isHoldingWeapon() {
@@ -201,30 +99,32 @@ public class AutoClicker extends Module {
                 mc.thePlayer.getCurrentEquippedItem().getItem() instanceof ItemSword;
     }
 
-    private boolean handleBlockBreaking() {
-        if (mc.objectMouseOver != null) {
+    public boolean breakBlock() {
+        if (breakBlocks.isToggled() && mc.objectMouseOver != null) {
             BlockPos p = mc.objectMouseOver.getBlockPos();
 
             if (p != null) {
                 if (mc.theWorld.getBlockState(p).getBlock() != Blocks.air &&
                         !(mc.theWorld.getBlockState(p).getBlock() instanceof BlockLiquid)) {
-                    if (!isBreakingBlock) {
-                        KeyBinding.setKeyBindState(LMB, true);
-                        KeyBinding.onTick(LMB);
-                        isBreakingBlock = true;
+                    if (!isHoldingBlockBreak) {
+                        KeyBinding.setKeyBindState(lmb, true);
+                        KeyBinding.onTick(lmb);
+                        isHoldingBlockBreak = true;
                     }
                     return true;
                 }
-                if (isBreakingBlock) {
-                    KeyBinding.setKeyBindState(LMB, false);
-                    isBreakingBlock = false;
+                if (isHoldingBlockBreak) {
+                    KeyBinding.setKeyBindState(lmb, false);
+                    isHoldingBlockBreak = false;
                 }
             }
         }
         return false;
     }
 
-    private boolean isTargetValid() {
+    public boolean hitSelectLogic() {
+        if (!hitSelect.isToggled())
+            return false;
         MovingObjectPosition result = mc.objectMouseOver;
         if (result != null && result.typeOfHit == MovingObjectPosition.MovingObjectType.ENTITY &&
                 result.entityHit instanceof EntityPlayer) {
@@ -234,22 +134,133 @@ public class AutoClicker extends Module {
         return false;
     }
 
-    @Override
-    public void onDisable() {
-        resetClicking();
-        if (blocking) {
-            KeyBinding.setKeyBindState(RMB, false);
-            blocking = false;
+    private void applyJitter() {
+        if (jitterAmount.getInput() > 0.0D) {
+            double jValue = jitterAmount.getInput() * 0.45D;
+            if (rand.nextBoolean()) {
+                mc.thePlayer.rotationYaw += rand.nextFloat() * jValue;
+            } else {
+                mc.thePlayer.rotationYaw -= rand.nextFloat() * jValue;
+            }
+
+            if (rand.nextBoolean()) {
+                mc.thePlayer.rotationPitch += rand.nextFloat() * jValue * 0.45D;
+            } else {
+                mc.thePlayer.rotationPitch -= rand.nextFloat() * jValue * 0.45D;
+            }
         }
     }
 
-    private void resetClicking() {
-        isBreakingBlock = false;
-        randomizedDelay = 1.0;
-        currentJitter = 0.0f;
-        if (mc.thePlayer != null) {
-            mc.thePlayer.rotationYaw -= currentJitter;
-            mc.thePlayer.rotationPitch -= currentJitter * 0.5f;
+    @SubscribeEvent
+    public void onRender(RenderHandEvent e) {
+        if (!PlayerUtils.isPlayerInGame() || mc.currentScreen != null) {
+            resetClickTimers();
+            return;
         }
+
+        // Left click processing only
+        if (Mouse.isButtonDown(0)) {
+            if (!shouldAttack())
+                return;
+            if (breakBlock())
+                return;
+
+            performClick();
+        } else {
+            resetClickTimers();
+        }
+    }
+
+    private void performClick() {
+        applyJitter();
+
+        long currentTime = System.currentTimeMillis();
+
+        if (nextPressTime == 0L || nextReleaseTime == 0L) {
+            updateClickDelay();
+            return;
+        }
+
+        if (currentTime > nextPressTime) {
+            KeyBinding.setKeyBindState(lmb, true);
+            KeyBinding.onTick(lmb);
+            HookUtils.setMouseButtonState(0, true);
+
+            if (blockHitChance.getInput() > 0.0 && rand.nextDouble() * 100 < blockHitChance.getInput()) {
+                if (mc.objectMouseOver != null &&
+                        mc.objectMouseOver.typeOfHit == MovingObjectPosition.MovingObjectType.ENTITY) {
+                    if (mc.thePlayer != null && mc.thePlayer.getCurrentEquippedItem() != null &&
+                            mc.thePlayer.getCurrentEquippedItem().getItem() instanceof ItemSword) {
+
+                        isBlockHitActive = true;
+
+                        KeyBinding.setKeyBindState(rmb, true);
+                        KeyBinding.onTick(rmb);
+                        HookUtils.setMouseButtonState(1, true);
+
+                        blockHitStartTime = System.currentTimeMillis();
+                    }
+                }
+            }
+
+            updateClickDelay();
+        } else if (currentTime > nextReleaseTime) {
+            KeyBinding.setKeyBindState(lmb, false);
+            HookUtils.setMouseButtonState(0, false);
+        }
+
+        if (isBlockHitActive) {
+            long blockHitDuration = System.currentTimeMillis() - blockHitStartTime;
+
+            double minDelay = minBlockHitDelay.getInput();
+            double maxDelay = maxBlockHitDelay.getInput();
+
+            if (minDelay > maxDelay) {
+                double temp = minDelay;
+                minDelay = maxDelay;
+                maxDelay = temp;
+            }
+
+            long holdTime = (long) (minDelay + (maxDelay - minDelay) * rand.nextDouble());
+
+            if (blockHitDuration >= holdTime) {
+                KeyBinding.setKeyBindState(rmb, false);
+                HookUtils.setMouseButtonState(1, false);
+                isBlockHitActive = false;
+            }
+        }
+    }
+
+    private void updateClickDelay() {
+        double cps = minCPS.getInput() + (maxCPS.getInput() - minCPS.getInput()) * rand.nextDouble() +
+                (0.4D * rand.nextDouble());
+
+        long delay = Math.round(1000.0D / cps);
+        long currentTime = System.currentTimeMillis();
+
+        if (currentTime > nextMultiplierUpdateTime) {
+            if (!multiplierActive && rand.nextInt(100) >= 85) {
+                multiplierActive = true;
+                delayMultiplier = 1.1D + rand.nextDouble() * 0.15D;
+            } else {
+                multiplierActive = false;
+                delayMultiplier = 1.0D;
+            }
+            nextMultiplierUpdateTime = currentTime + 500L + rand.nextInt(1500);
+        }
+
+        if (currentTime > nextExtraDelayUpdateTime) {
+            if (rand.nextInt(100) >= 80) {
+                delay += 50L + rand.nextInt(100);
+            }
+            nextExtraDelayUpdateTime = currentTime + 500L + rand.nextInt(1500);
+        }
+
+        if (multiplierActive) {
+            delay = (long) (delay * delayMultiplier);
+        }
+
+        nextPressTime = currentTime + delay;
+        nextReleaseTime = currentTime + (delay / 2L) - rand.nextInt(10);
     }
 }
