7b9ab7820df35a072376f5423b76ff18bbeaca3d
Francesco <100066266+cranci1@users.noreply.github.com>
2025-04-26 17:00:42 +0200

    yeah

--- src/main/java/dev/stormy/client/module/modules/gamemode/BedWars.java
+++ src/main/java/dev/stormy/client/module/modules/gamemode/BedWars.java
@@ -7,6 +7,7 @@ import dev.stormy.client.utils.player.PlayerUtils;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemSword;
 import net.minecraft.init.Items;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockBed;
@@ -22,6 +23,7 @@ import java.util.*;
 
 public class BedWars extends Module {
     public static TickSetting diamondArmor, fireball, obsidian, enderPearl, inv, shouldPing;
+    public static TickSetting diamondSword, ironSword;
     
     private final int OBSIDIAN_SCAN_RADIUS = 10;
     private final int SCAN_INTERVAL = 20;
@@ -30,6 +32,8 @@ public class BedWars extends Module {
     private List<String> armoredPlayers = new ArrayList<>();
     private Map<String, Item> lastHeldItems = new HashMap<>();
     private Map<BlockPos, Long> obsidianPositions = new HashMap<>();
+    private Set<String> diamondSwordPlayers = new HashSet<>();
+    private Set<String> ironSwordPlayers = new HashSet<>();
     
     private boolean outsideSpawn = true;
     private BlockPos spawnPos = null;
@@ -39,6 +43,8 @@ public class BedWars extends Module {
         super("BedWars", ModuleCategory.GameMode, 0);
         this.registerSetting(new DescriptionSetting("BedWars utilities and alerts"));
         this.registerSetting(diamondArmor = new TickSetting("Diamond Armor", true));
+        this.registerSetting(diamondSword = new TickSetting("Diamond Sword", true));
+        this.registerSetting(ironSword = new TickSetting("Iron Sword", true));
         this.registerSetting(fireball = new TickSetting("Fireball", true));
         this.registerSetting(obsidian = new TickSetting("Obsidian", true));
         this.registerSetting(enderPearl = new TickSetting("Ender Pearl", true));
@@ -63,6 +69,8 @@ public class BedWars extends Module {
         armoredPlayers.clear();
         lastHeldItems.clear();
         obsidianPositions.clear();
+        diamondSwordPlayers.clear();
+        ironSwordPlayers.clear();
     }
     
     @SubscribeEvent
@@ -87,11 +95,13 @@ public class BedWars extends Module {
             }
             
             for (EntityPlayer player : mc.theWorld.playerEntities) {
-                if (player == null || player == mc.thePlayer || PlayerUtils.isTeamMate(player))
+                if (player == null || player == mc.thePlayer || PlayerUtils.isTeamMate(player) || 
+                    player.isInvisibleToPlayer(mc.thePlayer) || isSpectator(player))
                     continue;
                     
                 String name = player.getName();
                 
+                // Check for diamond armor
                 if (diamondArmor.isToggled()) {
                     ItemStack leggings = player.getCurrentArmor(1);
                     ItemStack boots = player.getCurrentArmor(0);
@@ -105,10 +115,32 @@ public class BedWars extends Module {
                     }
                 }
                 
+                // Check for sword and items
                 ItemStack heldItem = player.getHeldItem();
                 if (heldItem != null) {
                     Item item = heldItem.getItem();
                     
+                    // Diamond sword check
+                    if (diamondSword.isToggled() && item instanceof ItemSword) {
+                        String materialName = ((ItemSword) item).getToolMaterialName();
+                        if (materialName.equals("EMERALD") && !diamondSwordPlayers.contains(name)) {
+                            diamondSwordPlayers.add(name);
+                            double distance = Math.round(mc.thePlayer.getDistanceToEntity(player));
+                            sendAlert(EnumChatFormatting.AQUA + player.getName() + " has a diamond sword! (" + 
+                                     EnumChatFormatting.DARK_PURPLE + (int)distance + "m" + EnumChatFormatting.GRAY + ")");
+                            pingPlayer();
+                        }
+                        // Iron sword check
+                        else if (ironSword.isToggled() && materialName.equals("IRON") && !ironSwordPlayers.contains(name)) {
+                            ironSwordPlayers.add(name);
+                            double distance = Math.round(mc.thePlayer.getDistanceToEntity(player));
+                            sendAlert(EnumChatFormatting.WHITE + player.getName() + " has an iron sword! (" + 
+                                     EnumChatFormatting.DARK_PURPLE + (int)distance + "m" + EnumChatFormatting.GRAY + ")");
+                            pingPlayer();
+                        }
+                    }
+                    
+                    // Other items check (fireball, ender pearl)
                     if (!lastHeldItems.containsKey(name) || lastHeldItems.get(name) != item) {
                         lastHeldItems.put(name, item);
                         
@@ -140,9 +172,10 @@ public class BedWars extends Module {
         }
     }
     
+    // Improved obsidian detection
     private void scanForObsidian() {
         for (EntityPlayer player : mc.theWorld.playerEntities) {
-            if (player == mc.thePlayer || PlayerUtils.isTeamMate(player))
+            if (player == null || player == mc.thePlayer || PlayerUtils.isTeamMate(player) || isSpectator(player))
                 continue;
                 
             BlockPos playerPos = new BlockPos(player.posX, player.posY, player.posZ);
@@ -151,11 +184,14 @@ public class BedWars extends Module {
                     for (int z = -OBSIDIAN_SCAN_RADIUS; z <= OBSIDIAN_SCAN_RADIUS; z++) {
                         BlockPos pos = playerPos.add(x, y, z);
                         Block block = mc.theWorld.getBlockState(pos).getBlock();
-                        if (block instanceof BlockObsidian && !obsidianPositions.containsKey(pos) && isNextToBed(pos)) {
-                            obsidianPositions.put(pos, System.currentTimeMillis());
-                            sendAlert(EnumChatFormatting.DARK_PURPLE + player.getName() + " placed obsidian!");
-                            pingPlayer();
-                            return;
+                        if (block instanceof BlockObsidian && !obsidianPositions.containsKey(pos)) {
+                            // Check if obsidian is close to a bed or within player's reach
+                            if (isNextToBed(pos) || player.getDistance(pos.getX(), pos.getY(), pos.getZ()) < 5.0) {
+                                obsidianPositions.put(pos, System.currentTimeMillis());
+                                sendAlert(EnumChatFormatting.DARK_PURPLE + player.getName() + " placed obsidian!");
+                                pingPlayer();
+                                return;
+                            }
                         }
                     }
                 }
@@ -166,8 +202,23 @@ public class BedWars extends Module {
     private boolean isNextToBed(BlockPos blockPos) {
         for (EnumFacing facing : EnumFacing.values()) {
             BlockPos offset = blockPos.offset(facing);
-            if (mc.theWorld.getBlockState(offset).getBlock() instanceof BlockBed) {
+            Block block = mc.theWorld.getBlockState(offset).getBlock();
+            if (block instanceof BlockBed) {
                 return true;
+            }
+        }
+        
+        // Check in a wider radius for beds
+        for (int x = -2; x <= 2; x++) {
+            for (int y = -1; y <= 1; y++) {
+                for (int z = -2; z <= 2; z++) {
+                    if (x == 0 && y == 0 && z == 0) continue;
+                    BlockPos pos = blockPos.add(x, y, z);
+                    Block block = mc.theWorld.getBlockState(pos).getBlock();
+                    if (block instanceof BlockBed) {
+                        return true;
+                    }
+                }
             }
         }
         return false;
@@ -189,7 +240,7 @@ public class BedWars extends Module {
     
     private void checkForInvisPotion() {
         for (EntityPlayer player : mc.theWorld.playerEntities) {
-            if (player == mc.thePlayer || PlayerUtils.isTeamMate(player))
+            if (player == mc.thePlayer || PlayerUtils.isTeamMate(player) || isSpectator(player))
                 continue;
                 
             if (player.isInvisible() && isPlayerInTabList(player)) {
@@ -209,6 +260,16 @@ public class BedWars extends Module {
         return mc.getNetHandler().getPlayerInfoMap().stream()
             .anyMatch(info -> info != null && info.getGameProfile() != null && 
                      info.getGameProfile().getName().equals(player.getName()));
+    }
+    
+    // Check if player is in spectator mode
+    private boolean isSpectator(EntityPlayer player) {
+        // Check if player is a spectator by seeing if they're invisible and not in combat
+        return (player.isInvisible() && player.isSpectator()) || 
+               (player.isInvisible() && player.getHealth() <= 0) ||
+               // Alternative check using GameType when available
+               (mc.getNetHandler() != null && mc.getNetHandler().getPlayerInfo(player.getUniqueID()) != null && 
+                mc.getNetHandler().getPlayerInfo(player.getUniqueID()).getGameType().isSpectator());
     }
     
     @SubscribeEvent
